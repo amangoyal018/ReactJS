4ec80d812adfc9d683489456e4180672814db85a
amangoyal018 <goyalamanmay18@gmail.com>
2024-04-13 18:31:48 +0530

    css

---
+++ 03counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+
+
+  let [counter , setCounter] = useState(15)
+
+  const addValue = () =>{
+    // counter = counter + 1; 
+    setCounter(counter + 1);
+    console.log("clicked",counter);
+    
+  }
+  const subValue = () =>{
+    // counter = counter + 1; 
+    setCounter(counter - 1);
+    console.log("clicked",counter);
+    
+  }
+  return (
+    <>
+      <h1>hello world</h1>
+      <h1>Counter Value : {counter}</h1>
+
+      <button
+      onClick={addValue} >Increment {counter}</button>
+      <br />
+      
+      <button
+      onClick={subValue}>Decrement {counter} </button>
+    </>
+  )
+}
+
+export default App
