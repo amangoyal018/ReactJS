469de90fc0ceb7504f098533e5d621495a4e9a4e
amangoyal018 <goyalamanmay18@gmail.com>
2024-04-17 11:55:39 +0530

    fdv

---
+++ 09context/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const UserContext = React.createContext()
+// it is a hook that allows you to pass data through the component tree without having to pass props down manually at every level.
+
+export deafult UserContext;
